1598876d2fb48d442d4bd79b82df8c1334d2ed21
Nitish Puri <npuri1903@gmail.com>
2025-02-22 08:33:31 +0530

    fin.

--- src/app.cpp
+++ src/app.cpp
@@ -67,10 +67,75 @@ int main()
     }
 
     glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    // Setup ImGui
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Quad Data
+    float vertices[] = {
+        -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, // Bottom-left (red)
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  // Bottom-right (green)
+        0.5f, 0.5f, 0.0f, 0.0f, 1.0f,   // Top-right (blue)
+        -0.5f, 0.5f, 1.0f, 1.0f, 0.0f   // Top-left (yellow)
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0};
+
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)(2 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    GLuint shaderProgram = createShaderProgram();
 
     while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
+
+        // Start ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // ImGui UI
+        ImGui::Begin("Control Panel");
+        static float color[3] = {1.0f, 1.0f, 1.0f};
+        ImGui::ColorEdit3("Quad Color", color);
+        ImGui::Text("Press ESC to exit.");
+        ImGui::End();
+
+        // Clear screen
+        glClearColor(color[0], color[1], color[2], 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Render Quad
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // Render ImGui
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
     }
 
