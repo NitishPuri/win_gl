e603958e6c7f288ced8d39fe5d73889d71b3f341
Nitish Puri <npuri1903@gmail.com>
2025-02-22 08:19:31 +0530

    add glad

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 GLFW Window scaffolding.
 
 This project is a template for a GLFW window project.
+
+https://github.com/ocornut/imgui?tab=readme-ov-file#getting-started--integration
+https://github.com/ocornut/imgui/wiki/Useful-Extensions
--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,54 @@
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include <iostream>
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
+// Vertex Shader
+const char *vertexShaderSrc = R"(   
+    #version 330 core
+    layout (location = 0) in vec2 aPos;
+    layout (location = 1) in vec3 aColor;
+    out vec3 vColor;
+    void main() {
+        vColor = aColor;
+        gl_Position = vec4(aPos, 0.0, 1.0);
+    }
+)";
+
+// Fragment Shader
+const char *fragmentShaderSrc = R"(
+    #version 330 core
+    in vec3 vColor;
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(vColor, 1.0);
+    }
+)";
+
+// Create a simple shader program
+GLuint createShaderProgram()
+{
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSrc, nullptr);
+    glCompileShader(fragmentShader);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
+}
 
 int main()
 {
@@ -9,7 +58,7 @@ int main()
         return -1;
     }
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "GLFW Window", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
     if (!window)
     {
         std::cerr << "Failed to create GLFW window\n";
